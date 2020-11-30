615f831a3b3853615923e137d9e9595c222d9dc8
Toby Boudreaux <tobynboudreaux@gmail.com>
2020-11-29 19:11:19 -0600

    Tests working

--- src/test/java/UsersTest.java
+++ src/test/java/UsersTest.java
@@ -1,18 +1,18 @@
-// import com.revature.amazon.controller.Users;
+import com.revature.amazon.controller.Users;
 
-// import static io.restassured.RestAssured.get;
-// import static org.hamcrest.CoreMatchers.equalTo;
+import static io.restassured.RestAssured.get;
+import static org.hamcrest.CoreMatchers.equalTo;
  
-// import org.junit.Test;
+import org.junit.Test;
 
-// public class UsersTest { 
+public class UsersTest { 
     
-//     @Test
-//     public void testUserFetchesSuccess() {
-//         get("http://localhost:4321/users?id=1")
-//         .then()
-//         .body("id", equalTo(1))
-//         .body("email", equalTo("toby@gmail.com"));
-//     }
+    @Test
+    public void testUserFetchesSuccess() {
+        get("http://localhost:4321/users?id=1")
+        .then()
+        .body("id", equalTo(1))
+        .body("email", equalTo("toby@gmail.com"));
+    }
 
-// }
+}
